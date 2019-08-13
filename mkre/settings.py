5a20fda79eb134485c9cea1c4e803ba7241468e4
Manikanta2 <manikanta1596@gmail.com>
2019-08-13 03:16:04 -0700

    created models for listings and realtors and linked PostgreSQL database

--- mkre/settings.py
+++ mkre/settings.py
@@ -128,3 +128,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mkre/static')
 ]
+
+
+# MEDIA FOLDER SETTINGS
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
