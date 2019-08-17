6fdee28262a90d90cdd114aacd286a00630de550
Manikanta2 <manikanta1596@gmail.com>
2019-08-17 01:37:54 -0700

    Added listings view with a bootstrap template

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,21 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
+from .models import Listing
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    paginator = Paginator(listings, 1)
+    page = request.GET.get('page')
+    page_listings = paginator.get_page(page)
+    context = {
+        'listings': page_listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
